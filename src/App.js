4df909dd58ab629022adb981cc55596d1178d34a
Nick Toye <nick.toye@sofology.co.uk>
2018-06-29 13:58:06 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+import Grid from '@material-ui/core/Grid';
+
+import EnhancedTable from './components/EnhancedTable';
+import SearchBar from './components/SearchBar';
+
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#a0f6ff',
+      main: '#6ac3f3',
+      dark: '#2d93c0',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff8e61',
+      main: '#de5e35',
+      dark: '#a62d08',
+      contrastText: '#fff',
+    },
+  },
+});
+
+class App extends Component {
+  
+  render() {
+    return (
+      <React.Fragment>
+        <Grid container spacing={24}>
+          <Grid item xs={12}>
+            <MuiThemeProvider theme={theme}>
+              <SearchBar />
+              <EnhancedTable />
+            </MuiThemeProvider>
+          </Grid>
+        </Grid>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
